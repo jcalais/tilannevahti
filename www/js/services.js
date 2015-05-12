2dcccf6933407474589ff11d85b4bc4426a89ab8
Joakim Calais <joakim.calais@gmail.com>
2015-05-12 15:23:20 +0300

    Added support for toggling sources

--- www/js/services.js
+++ www/js/services.js
@@ -224,31 +224,34 @@ angular.module('starter.services', [])
       return sourcelist;
     },
 
-    // Return events by type.
-    getBySource: function(source) {
+    // Return events by type and source.
+    getByType: function(type) {
       var ret = [];
+      var activetype;
+      var activesource;
+
       for (var i=0 ; i<events.length ; i++) {
-        if (events[i].source == source || !source) {
-          events[i].icon = getIconForSource(events[i].source);
+        activetype = false;
+        activesource = false;
+
+        events[i].icon = getIconForSource(events[i].source);
+        for (var j=0 ; j<typelist.length ; j++) {
+          if (typelist[j].enabled && events[i].type == typelist[j].label) {
+            activetype = true;
+          }
+        }
+
+        for (var j=0 ; j<sourcelist.length ; j++) {
+          if (sourcelist[j].enabled && events[i].source == sourcelist[j].label) {
+            activesource = true;
+          }
+        }
+
+        if (activetype && activesource) {
           ret.push(events[i]);  
         }
       }
-      return ret;
-    },
 
-    // Return events by type.
-    getByType: function(type) {
-      var ret = [];
-      var activetypes = [];
-      
-      for (var i=0 ; i<events.length ; i++) {
-        for (var j=0 ; j<typelist.length ; j++) {
-          if (typelist[j].enabled && events[i].type == typelist[j].label) {
-            events[i].icon = getIconForType(events[i].type);
-            ret.push(events[i]);  
-          }
-        }
-      }
       return ret;
     },
 
@@ -299,53 +302,4 @@ angular.module('starter.services', [])
       }
     }
   }
-})
-
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-  
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  },{
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  };
 });
